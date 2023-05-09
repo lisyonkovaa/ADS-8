4255816dd9238409f24c3b0ff617af295d814654
lisyonkovaa <114025777+lisyonkovaa@users.noreply.github.com>
2023-05-09 10:33:43 +0000

    upd3 b189

--- src/train.cpp
+++ src/train.cpp
@@ -21,13 +21,38 @@ void Train::addCage(bool light) {
 }
 
 int Train::getLength() {
-    int length = 0;
-    Cage *current = first;
-    do {
+    int length = 1;
+    Cage* current = first;
+    if (!current->light) {
+        current->light = true;
+    }
+    current = current->next;
+    countOp++;
+    while (true) {
+        if (!current->light) {
         length++;
         current = current->next;
-    } while (current != first);
-        return length;
+        countOp++;
+        } else {
+            current->light = false;
+            int temporary_length = length;
+            while (length) {
+                current = current->prev;
+                length--;
+                countOp++;
+            }
+            if (!current->light) {
+                return temporary_length;
+            } else {
+                while (temporary_length) {
+                    current = current->next;
+                    temporary_length--;
+                    length++;
+                    countOp++;
+                }
+            }
+        }
+    }
 }
 
 int Train::getOpCount() {
